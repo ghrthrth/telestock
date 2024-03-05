f71b6bb273b706ac80fb3947262e34e6eaf2115f
ghrthrth <dima.skorohodov01@mail.ru>
2024-03-06 00:20:51 +0300

    change any functionals

---
+++ app/src/main/java/com/example/massage_parlor/ui/slideshow/ImageAdapters.java
@@ -0,0 +1,112 @@
+package com.example.massage_parlor.ui.slideshow;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.massage_parlor.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ImageAdapters extends BaseAdapter implements Filterable {
+
+    private Context mContext;
+    private List<String> mPhotoUrls;
+    private List<String> mTitles;
+    private List<String> mDescriptions;
+    private List<String> mFilteredTitles; // Добавьте отфильтрованные заголовки
+    private LayoutInflater mInflater;
+    private ItemFilter mItemFilter = new ItemFilter();
+
+    public ImageAdapters(Context context, List<String> photoUrls, List<String> titles, List<String> descriptions) {
+        mContext = context;
+        mPhotoUrls = photoUrls;
+        mTitles = titles;
+        mDescriptions = descriptions;
+        mFilteredTitles = new ArrayList<>(titles); // Инициализируйте отфильтрованные заголовки
+        mInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return mFilteredTitles.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.grid_item_layouts, parent, false);
+        }
+
+        // Находим представления внутри макета элемента сетки
+        ImageView imageView = convertView.findViewById(R.id.grid_image);
+        TextView titleTextView = convertView.findViewById(R.id.title_text_view);
+        TextView descriptionTextView = convertView.findViewById(R.id.description_text_view);
+
+        // Устанавливаем данные для каждого представления
+        String title = mFilteredTitles.get(position);
+        int originalPosition = mTitles.indexOf(title); // Получаем позицию в оригинальном списке
+
+        String photoUrl = mPhotoUrls.get(originalPosition);
+        String description = mDescriptions.get(originalPosition);
+
+        // Загружаем изображение с помощью библиотеки Picasso или Glide
+        Picasso.get().load(photoUrl).into(imageView);
+
+        // Устанавливаем текст для текстовых представлений
+        titleTextView.setText(title);
+        descriptionTextView.setText(description);
+
+        return convertView;
+    }
+
+    @Override
+    public Filter getFilter() {
+        return mItemFilter;
+    }
+
+    private class ItemFilter extends Filter {
+
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            String filterString = constraint.toString().toLowerCase();
+
+            FilterResults results = new FilterResults();
+            List<String> filteredList = new ArrayList<>();
+
+            for (String title : mTitles) {
+                if (title.toLowerCase().contains(filterString)) {
+                    filteredList.add(title);
+                }
+            }
+
+            results.values = filteredList;
+            results.count = filteredList.size();
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            mFilteredTitles = (List<String>) results.values;
+            notifyDataSetChanged();
+        }
+    }
+}
--- app/src/main/java/com/example/massage_parlor/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/massage_parlor/ui/slideshow/SlideshowFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +14,22 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.massage_parlor.databinding.FragmentSlideshowBinding;
+import com.example.massage_parlor.ui.gallery.ImageAdapter;
+import com.example.massage_parlor.ui.gallery.ProductDetailFragment;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class SlideshowFragment extends Fragment {
 
@@ -23,12 +42,92 @@ public class SlideshowFragment extends Fragment {
 
         binding = FragmentSlideshowBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-        final TextView textView = binding.textSlideshow;
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        getPhotoUrlsFromServer();
         return root;
     }
 
+    private void addItemsToList(JSONArray jsonArray, List<String> list) {
+        for (int i = 0; i < jsonArray.length(); i++) {
+            try {
+                String item = jsonArray.getString(i);
+                list.add(item);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void getPhotoUrlsFromServer() {
+        String url = "https://claimbe.store/massage_parlor/api/return_news.php"; // Замените на ваш URL-адрес сервера
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    String json = response.body().string();
+                    try {
+                        JSONObject jsonObject = new JSONObject(json);
+                        JSONArray idArray = jsonObject.getJSONArray("id");
+                        JSONArray photoUrlsArray = jsonObject.getJSONArray("photoUrls");
+                        JSONArray titleArray = jsonObject.getJSONArray("title");
+                        JSONArray descriptionArray = jsonObject.getJSONArray("description");
+
+                        List<String> ids = new ArrayList<>();
+                        List<String> photoUrls = new ArrayList<>();
+                        List<String> titles = new ArrayList<>();
+                        List<String> descriptions = new ArrayList<>();
+
+
+                        addItemsToList(idArray, ids);
+                        addItemsToList(photoUrlsArray, photoUrls);
+                        addItemsToList(titleArray, titles);
+                        addItemsToList(descriptionArray, descriptions);
+
+                        displayPhotosInGrid(ids, photoUrls, titles, descriptions);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    private void displayPhotosInGrid(List<String> ids, List<String> photoUrls, List<String> titles, List<String> descriptions) {
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                GridView gridView = binding.gridView1;
+                ImageAdapters adapters = new ImageAdapters(getContext(), photoUrls, titles, descriptions);
+                gridView.setAdapter(adapters);
+                SearchView searchView = binding.searchView;
+                searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+                    @Override
+                    public boolean onQueryTextSubmit(String query) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onQueryTextChange(String newText) {
+                        adapters.getFilter().filter(newText);
+                        return true;
+                    }
+
+                });
+            }
+        });
+    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
